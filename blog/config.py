1ecb04d125ecd9e6a40ae21d035382f33ef43773
Rahul Patil <81096780+RahulPatil-Tech@users.noreply.github.com>
2025-07-28 20:25:22 +0530

    Delete blog directory

--- blog/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rp32:Strong%40123@localhost/blog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret')
