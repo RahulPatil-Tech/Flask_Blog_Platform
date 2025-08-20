3b6a834387959fa0d03f8a9ea7f70ed7e288d24c
Rahul Patil <81096780+RahulPatil-Tech@users.noreply.github.com>
2025-08-20 11:41:27 +0530

    Delete blog directory

--- blog/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SECRET_KEY = 'this-should-be-secret-and-random'
-
-    # URL-encoded password: '@' becomes '%40'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rp32:Strong%40123@localhost/blog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
