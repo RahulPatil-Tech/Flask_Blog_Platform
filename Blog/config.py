78b0ce2eeb5dd6453dc41721de9f0e5567e84982
Rahul Patil <81096780+RahulPatil-Tech@users.noreply.github.com>
2026-01-19 14:00:43 +0530

    Update config.py

--- Blog/config.py
+++ Blog/config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     SECRET_KEY = 'this-should-be-secret-and-random'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:hNolfQXzqIDgNMJGpcoiQoPHXkxEKiKq@mysql.railway.internal:3306/railway'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:hNolfQXzqIDgNMJGpcoiQoPHXkxEKiKq@switchyard.proxy.rlwy.net:21244/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail settings
